482bb53232f93aa081105d6a0565698b6f177537
sunjit22 <63877211+sunjit22@users.noreply.github.com>
2021-12-12 17:56:53 -0500

    adding repo

---
+++ consts.js
@@ -0,0 +1,8 @@
+// const MONGODB_URL = 'mongodb://localhost:27017/tuiter';
+// const MONGODB_URL = 'mongodb+srv://arushia442:clinifind@cluster0.u3bdo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+// const MONGODB_URL = 'mongodb+srv://sunjit22:clinifind123@cluster0.tnksa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const MONGODB_URL = 'mongodb+srv://sunjit22:clinifind123@cluster0.tnksa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+module.exports = {
+  MONGODB_URL
+};
